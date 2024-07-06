5c2c2fc7b19c4ab2d3242947bead9ee1146cc369
Samarth Dadhaniya <samarthdadhaniya@Samarths-MacBook-Air.local>
2024-07-06 22:37:02 +0530

    Update code for Portfolio.

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import Contact from "./components/Contact";
+import CustomCursor from "./components/CustomCursor";
+import Hero from "./components/Hero";
+import Projects from "./components/Projects";
+import TechStack from "./components/TechStack";
+
+function App() {
+  return (
+    <>
+      <CustomCursor />
+      <Hero />
+      <Projects />
+      <TechStack />
+      <Contact />
+    </>
+  );
+}
+
+export default App;
